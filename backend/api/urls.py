9fc43122e6ae5ef71d062402423481d843d6d66b
Emmanuel <emma145@hotmail.es>
2024-11-08 20:44:46 -0500

    feat :sparkles: add all CRUD operations for devices

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import ProfessionalListAPIView, ProfessionalCreateAPIView, ProfessionalDestroyAPIView, \
-    ProfessionalRetrieveAPIView, ProfessionalUpdateAPIView
+    ProfessionalRetrieveAPIView, ProfessionalUpdateAPIView, DeviceListAPIView, DeviceCreateAPIView, \
+    DeviceDestroyAPIView, DeviceUpdateAPIView, DeviceRetrieveAPIView
 
 urlpatterns = [
     path('professionals/', ProfessionalListAPIView.as_view(), name='professionals-list'),
@@ -8,4 +9,9 @@ urlpatterns = [
     path('professionals/delete/<int:pk>/', ProfessionalDestroyAPIView.as_view(), name='professionals-delete'),
     path('professionals/<int:pk>/', ProfessionalRetrieveAPIView.as_view(), name='professionals-retrieve'),
     path('professionals/update/<int:pk>/', ProfessionalUpdateAPIView.as_view(), name='professionals-update'),
+    path('devices/', DeviceListAPIView.as_view(), name='devices-list'),
+    path('devices/create/', DeviceCreateAPIView.as_view(), name='devices-create'),
+    path('devices/delete/<int:pk>/', DeviceDestroyAPIView.as_view(), name='devices-delete'),
+    path('devices/<int:pk>/', DeviceRetrieveAPIView.as_view(), name='devices-retrieve'),
+    path('devices/update/<int:pk>/', DeviceUpdateAPIView.as_view(), name='devices-update')
 ]
