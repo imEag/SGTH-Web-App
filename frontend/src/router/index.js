2abc8c6ed0c0f05546e226b82a743d1c339e9975
Sebastian Zapata Rodríguez <sebastian.zapata17@udea.edu.co>
2024-11-10 20:36:27 -0500

    Update- professional

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,40 +1,46 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import {createRouter, createWebHistory} from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import ProfessionalsView from '@/views/ProfessionalsView.vue';
 import NewResponsable from '@/views/NewResponsable.vue';
 import EquipamentList from '@/views/EquipamentList.vue';
 import NewEquipament from '@/views/NewEquipament.vue';
+import ProfessionalView from "@/views/ProfessionalView.vue";
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/profesionales',
-      name: 'profesionales',
-      component: ProfessionalsView,
-    },
-    {
-      path: '/new-professional',
-      name: 'new-professional',
-      component: NewResponsable,
-    },
-    {
-      path: '/devices-list',
-      name: 'EquipamentList',
-      component: EquipamentList,
-    },
-    {
-      path: '/devices-create',
-      name: 'NewEquipament',
-      component: NewEquipament,
-    },
-  ],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView,
+        },
+        {
+            path: '/profesionales',
+            name: 'profesionales',
+            component: ProfessionalsView,
+        },
+        {
+            path: '/new-professional',
+            name: 'new-professional',
+            component: NewResponsable,
+        },
+        {
+            path: '/devices-list',
+            name: 'EquipamentList',
+            component: EquipamentList,
+        },
+        {
+            path: '/devices-create',
+            name: 'NewEquipament',
+            component: NewEquipament,
+        },
+        {
+            path: '/professional/:id',
+            name: 'professional',
+            component: ProfessionalView,
+        }
+    ],
 });
 
 export default router;
